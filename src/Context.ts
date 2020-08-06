5327a089a56e9402b44ad73345a8d57eb8ed5875
Kirill Zamotaev <k.zamotaev@corp.vk.com>
2020-08-06 23:27:52 +0300

    VKUI-15 context module implemented

--- src/Context.ts
+++ src/Context.ts
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Route } from 'router5';
- 
+import React from "react";
+import { Route } from "router5";
+import { OnTransitionParams } from './interfaces';
 export interface NavigatorContextProps {
-    [key:string]: any,
-    router?: any;
-    previousRoute?: Route;
-    activeView?: string;
-    activePanels?: any;
-    onRootTransition?: (OnTransitionParams:any) => void;
-    onTransition?: (OnTransitionParams:any) => void;
-    history?: Record<string, string[]>;
+  [key: string]: any;
+  router?: any;
+  previousRoute?: Route;
+  activeView?: string;
+  activePanels?: any;
+  activeModals?: any;
+  onRootTransition?: (OnTransitionParams: OnTransitionParams) => void;
+  onTransition?: (OnTransitionParams: OnTransitionParams) => void;
+  history?: Record<string, string[]>;
 }
-  
+
 export const NavigatorContext = React.createContext<NavigatorContextProps>({});

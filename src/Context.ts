c4f28f91802d86edf51d363d9e725a9afb1bef71
Kirill Zamotaev <k.zamotaev@corp.vk.com>
2020-08-08 01:02:06 +0300

    VKUI-15 extended context interface

--- src/Context.ts
+++ src/Context.ts
@@ -1,16 +1,18 @@
 import React from "react";
-import { Route } from "router5";
-import { OnTransitionParams } from './interfaces';
+import { Router } from "router5/dist/types/router";
+import { OnTransitionParams, Go } from './interfaces';
 export interface NavigatorContextProps {
-  [key: string]: any;
-  router?: any;
-  previousRoute?: Route;
+  [key:string]: any,
+  router?: Partial<Router>
+  previousRoute?: string;
   activeView?: string;
-  activePanels?: any;
-  activeModals?: any;
-  onRootTransition?: (OnTransitionParams: OnTransitionParams) => void;
-  onTransition?: (OnTransitionParams: OnTransitionParams) => void;
+  activePanels?: { [key:string]: string };
+  activeModals?: string[];
+  params?: any,
+  onRootTransition?: (onTransitionParams: OnTransitionParams) => void;
+  onTransition?: (onTransitionParams: OnTransitionParams) => void;
   history?: Record<string, string[]>;
+  go?: Go
 }
 
 export const NavigatorContext = React.createContext<NavigatorContextProps>({});

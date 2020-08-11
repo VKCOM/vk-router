0b07e353f8385b194864564a2e690771c1ff007a
Kirill Zamotaev <k.zamotaev@corp.vk.com>
2020-08-11 16:29:13 +0300

    VKUI-15 rebuild navigator context interface

--- src/Context.ts
+++ src/Context.ts
@@ -1,16 +1,4 @@
 import React from "react";
-import { Router } from "router5/dist/types/router";
-import { Go } from './interfaces';
+import { NavigatorState } from './Navigator';
 
-export interface NavigatorContextProps { 
-  router?: Partial<Router>
-  currentRoute?: any;
-  previousRoute?: any;
-  options?:any;
-  params?: any; 
-  history?: Record<string, string[]>;
-  go?: Go,
-  back?: VoidFunction,
-}
-
-export const NavigatorContext = React.createContext<NavigatorContextProps>({});
+export const NavigatorContext = React.createContext<Partial<NavigatorState>>({});

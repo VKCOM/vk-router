2a1682c890125908c06d6dce8712bd3d9078a4a8
Kirill <k.zamotaev@corp.vk.com>
2020-10-04 20:17:09 +0300

    VKUI-15 removed unnecessary space

--- src/hoc/withNavigator.tsx
+++ src/hoc/withNavigator.tsx
@@ -9,7 +9,6 @@ export function withNavigator<T>(Component: React.ComponentType<T>) {
     return class _Navigator extends React.PureComponent {
       public static contextType = NavigatorContext;
       public render() { 
-     
         const props = {
           ...this.props,
           ...this.context,

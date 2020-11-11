e127384a197f0fab52f0170897d269333bf503e9
Kirill <k.zamotaev@corp.vk.com>
2020-11-11 18:43:40 +0300

    VKUI-15 added preserve hash option

--- src/types.ts
+++ src/types.ts
@@ -12,6 +12,7 @@ export interface NavigatorConfig {
   persistentParams?: string[];
   subRouteKey?: string;
   routeKey?: string;
+  preserveHash?: boolean;
   errorLogger?: NavigatorErrorLogger;
 }
 
